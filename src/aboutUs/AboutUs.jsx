a541982bebf1e0821da88d8dc4dd641dc9e75808
bibielll <xooos110@gmail.com>
2023-05-29 10:41:31 +0700

    aboutus

--- src/aboutUs/AboutUs.jsx
+++ src/aboutUs/AboutUs.jsx
@@ -1,45 +1,40 @@
 import React from "react";
 import ContactItem from "./ContactItem";
 import "./AboutUs.css";
-import arip from "./poto/arip.jpg"
-import habil from "./poto/habil.jpeg"
-import rici from "./poto/rici.jpeg"
-import zita from "./poto/arip.jpg"
+import arip from "./poto/arip.jpg";
+import habil from "./poto/habil.jpeg";
+import rici from "./poto/rici.jpeg";
+import zita from "./poto/arip.jpg";
 
 const Contact = [
   {
     name: "Aripp",
     imgURL: arip,
-    NIM:"NIM: 00000073943",
-    desc:
-      "Bujug sang destroyer",
-    desc2:"Disebelah kanan saya, atasan saya →"
+    NIM: "NIM: 00000073943",
+    desc: "Bujug sang destroyer",
+    desc2: "Disebelah kanan saya, atasan saya →",
   },
   {
     name: "Habieell",
-    imgURL:habil,
-      NIM:"NIM: 00000073909",
-    desc:
-      "Sang legenda link, penemu, pencetus rumus Error = Ga tidur.",
-    desc2:"😪😪😪😪😪"
+    imgURL: habil,
+    NIM: "NIM: 00000073909",
+    desc: "Sang legenda link, penemu, pencetus rumus Error = Ga tidur.",
+    desc2: "😪😪😪😪😪",
   },
   {
     name: "Zitaa",
-    imgURL:
-      "",
-      NIM:"NIM: 00000075267",
-    desc:
-      "--",
-    desc2:""
+    imgURL: "",
+    NIM: "NIM: 00000075267",
+    desc: "--",
+    desc2: "",
   },
   {
     name: "Ricii",
     imgURL: rici,
-      NIM:"NIM: 00000074763",
-    desc:
-      "---",
-    desc2:""
-  }
+    NIM: "NIM: 00000074763",
+    desc: "aaaaaaaaaaaa",
+    desc2: "Akhirnya bisa tidurrr nyenyak",
+  },
 ];
 
 function AboutUs() {
