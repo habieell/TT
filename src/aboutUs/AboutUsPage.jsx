cb1ccef844584fba559982731d7bbfc4da712bed
bibielll <xooos110@gmail.com>
2023-05-30 13:15:08 +0700

    jsnjasd

--- src/aboutUs/AboutUsPage.jsx
+++ src/aboutUs/AboutUsPage.jsx
@@ -1,22 +1,14 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AboutUs from './AboutUs';
-import logo from './Logo.jpeg';
-import './AboutUs.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AboutUs from "./AboutUs";
+import logo from "./Logo.jpeg";
+import "./AboutUs.css";
 
 function AboutUsPage({ toggleNavbar }) {
   const navigate = useNavigate();
 
-  const handleLogoClick = () => {
-    toggleNavbar(true); 
-    navigate('/');
-  };
-
   return (
     <div className="about-us-page">
-      <div className="logo-container" onClick={handleLogoClick}>
-        <img src={logo} alt="Logo" />
-      </div>
       <AboutUs />
     </div>
   );
